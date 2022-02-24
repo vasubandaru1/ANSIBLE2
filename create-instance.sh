8059583b0ee885bce5875fd7c209910b7f62a6bb
vasu <bandaruvasu1@gmail.com>
2022-02-24 12:57:45 +0530

    cli  variables

--- create-instance.sh
+++ create-instance.sh
@@ -41,7 +41,7 @@ sleep 5
 IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | xargs)
 
 sed -e "s/DNSNAME/$1-dev.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id Z00401548BMH7OHSQWPB --change-batch file:///tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id Z0917209HV543MHNGHQH --change-batch file:///tmp/record.json
 
 
 }
