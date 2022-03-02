688118e692c74e8be978b7cd6e50c8a078883737
vasu <bandaruvasu1@gmail.com>
2022-03-02 10:20:57 +0530

    cli  variables

--- create-instance.sh
+++ create-instance.sh
@@ -28,7 +28,7 @@ CREATE() {
 
 if [ $COUNT -eq 0 ]; then
 
-aws ec2 run-instances --launch-template LaunchTemplateId=lt-0c2f23ab38ce47240,Version=02 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
+aws ec2 run-instances --launch-template LaunchTemplateId=lt-0c2f23ab38ce47240,Version=03 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
 
 else
   echo -e "\e[1;33m$1 Instance already exists\e[0m"
